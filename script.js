6ad6505eaf4c532e5c923d5a23488522f9dfccfd
Shafia Mohammad Rafiq <64019514+Shafia-Mohammad-Rafiq03@users.noreply.github.com>
2020-07-25 20:52:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function ChooseLevelEasy() {
+    alert('Hellow this is level easy');
+
+}
+
+function ChooseLevelMedium() {
+    alert('Hellow this is level medium');
+
+}
+
+function ChooseLevelHard() {
+    alert('Hellow this is level hard');
+
+}
+
+function confirmBox() {
+    if (confirm('Are you sure you want to Exit?')) {
+        window.location.href = '../index'
+    }
+}
